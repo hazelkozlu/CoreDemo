facd2cf00ecd0cd99ca76c064e5fd20d9d06ba5c
hazel <hazel@DESKTOP-SHEVOM8>
2021-10-31 21:51:20 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,41 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        //context class ı Dbcontext classından  miras alır.  entityframworkcore eklenmelidir.
+        //onnconfiguring adında bir methot tanımlanır ve paremetre olarak DbContextOptionsBuilder türünde
+        //optionsBuilder adında parametre alır.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("server=DESKTOP-SHEVOM8; database=CoreBlogDB; integrated security=true;");
+        }
+        //     sınıf        property
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        //neden migration oluşturuyoruz. Context sınıfında çağırmış olduğum dbsetleri yine context sınıfında oluşturmuş
+        //olduğum connectionstring üzerinden code first yaklaşımı ile veri tabanına yansıtmak istiyorum.
+        //package manager console da default project seçilmiş olması gerekir çünkü bizim veritabanı bağlantı stringi ve bunun tutulduğu sınıf dataaccesslayer içerisinde.
+        /*
+          4 tane entityframeworkcore paketi core,tools,desing,sqlserver hem dataaccesslayer a hem UI yani projenin kendisine eklenir.
+           presentationlayer CoreDemoya yani
+         * PM> add-migration mig1
+          Build started...
+          Build succeeded.
+          To undo this action, use Remove-Migration.
+         PM> update-database*/
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        //küçük resim
+        public string BlogThumbNailImage{ get; set; }
+        //normal resim
+        public string BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+        //bir Category ye blog yapmak için 
+        public int CategoryID { get; set; }
+        public Category Category  { get; set; }
+        //bir bloga birden fazla yorum yapılabilir.
+        public List<Comment> Comments{ get; set; }
+
+    }
+
+}
