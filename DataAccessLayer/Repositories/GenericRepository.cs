105acf0420d5ae172344d3e0cfed19d271bae86b
hazelkozlu43@gmail.com <hazelkozlu43@gmail.com>
2021-11-03 21:58:43 +0300

    BlogReadAll olarak düzenlendi. blog devamını oku sayfası eklendi

--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,11 +30,18 @@ namespace DataAccessLayer.Repositories
             return c.Set<T>().ToList();
         }
 
+    
         public void Insert(T t)
         {
             using var c = new Context();
             c.Add(t);
             c.SaveChanges();
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            using var c = new Context();
+            return c.Set<T>().Where(filter).ToList();
         }
 
         public void Update(T t)
